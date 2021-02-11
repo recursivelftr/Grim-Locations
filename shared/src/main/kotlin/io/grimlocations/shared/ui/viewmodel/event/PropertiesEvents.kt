a6ca2d941feb2038eda43fb1041d9671241a0476
recursiveg87 <recursiveg87@gmail.com>
2021-02-11 07:54:16 -0700

    adding properties screen validation

--- shared/src/main/kotlin/io/grimlocations/shared/ui/viewmodel/event/PropertiesEvents.kt
+++ shared/src/main/kotlin/io/grimlocations/shared/ui/viewmodel/event/PropertiesEvents.kt
@@ -1,10 +1,14 @@
 package io.grimlocations.shared.ui.viewmodel.event
 
 import io.grimlocations.shared.ui.viewmodel.PropertiesViewModel
-import io.grimlocations.shared.ui.viewmodel.reducer.persistPropertiesState
-import io.grimlocations.shared.ui.viewmodel.reducer.updatePropertiesInstallPath
-import io.grimlocations.shared.ui.viewmodel.reducer.updatePropertiesSavePath
+import io.grimlocations.shared.ui.viewmodel.reducer.*
+import io.grimlocations.shared.ui.viewmodel.reducer.addPropertiesStateErrors
+import io.grimlocations.shared.ui.viewmodel.state.PropertiesStateError.*
 import kotlinx.coroutines.launch
+import org.apache.logging.log4j.LogManager
+import java.io.File
+
+val logger = LogManager.getLogger()
 
 fun PropertiesViewModel.persistState() {
     viewModelScope.launch {
@@ -15,6 +19,12 @@ fun PropertiesViewModel.persistState() {
 fun PropertiesViewModel.updateInstallPath(path: String) {
     viewModelScope.launch {
         stateManager.updatePropertiesInstallPath(path)
+
+        if (isValidInstallPath(path)) {
+            stateManager.removePropertiesStateErrors(GRIM_INTERNALS_NOT_FOUND)
+        } else {
+            stateManager.addPropertiesStateErrors(GRIM_INTERNALS_NOT_FOUND)
+        }
     }
 }
 
@@ -22,4 +32,23 @@ fun PropertiesViewModel.updateSavePath(path: String) {
     viewModelScope.launch {
         stateManager.updatePropertiesSavePath(path)
     }
+}
+
+private fun isValidInstallPath(path: String): Boolean {
+    try {
+        File(path).listFiles { it: File -> it.isFile }?.let {
+            return it.any { f -> f.name.equals("GrimInternals64.exe", ignoreCase = true) }
+        } ?: run {
+            logger.error("Path is either not a directory or an I/O error has occurred.")
+        }
+    } catch (e: SecurityException) {
+        logger.error("Read access is denied to this directory: $path", e)
+    }
+
+    return false
+}
+
+private fun isValidSavePath(path: String): Boolean {
+
+    return false
 }
--- shared/src/main/kotlin/io/grimlocations/shared/ui/viewmodel/reducer/PropertiesReducers.kt
+++ shared/src/main/kotlin/io/grimlocations/shared/ui/viewmodel/reducer/PropertiesReducers.kt
@@ -6,6 +6,8 @@ import io.grimlocations.shared.framework.ui.getState
 import io.grimlocations.shared.framework.ui.setState
 import io.grimlocations.shared.ui.GLStateManager
 import io.grimlocations.shared.ui.viewmodel.state.PropertiesState
+import io.grimlocations.shared.ui.viewmodel.state.PropertiesStateError
+import io.grimlocations.shared.ui.viewmodel.state.PropertiesStateWarning
 import io.grimlocations.shared.util.guardLet
 
 suspend fun GLStateManager.loadPropertiesState() {
@@ -33,4 +35,24 @@ suspend fun GLStateManager.updatePropertiesInstallPath(path: String) {
 
 suspend fun GLStateManager.updatePropertiesSavePath(path: String) {
     setState(getState<PropertiesState>().copy(savePath = path))
+}
+
+suspend fun GLStateManager.addPropertiesStateErrors(vararg errors: PropertiesStateError) {
+    val state = getState<PropertiesState>()
+    setState(state.copy(errors = setOf(*errors, *state.errors.toTypedArray())))
+}
+
+suspend fun GLStateManager.removePropertiesStateErrors(vararg errors: PropertiesStateError) {
+    val state = getState<PropertiesState>()
+    setState(state.copy(errors = state.errors.filter { !errors.contains(it) }.toSet()))
+}
+
+suspend fun GLStateManager.addPropertiesStateWarnings(vararg warnings: PropertiesStateWarning) {
+    val state = getState<PropertiesState>()
+    setState(state.copy(warnings = setOf(*warnings, *state.warnings.toTypedArray())))
+}
+
+suspend fun GLStateManager.removePropertiesStateWarnings(vararg warnings: PropertiesStateWarning) {
+    val state = getState<PropertiesState>()
+    setState(state.copy(warnings = state.warnings.filter { !warnings.contains(it) }.toSet()))
 }
