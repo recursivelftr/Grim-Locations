5be71eaf4d5e6bff7d03f05fe37ebac23f8a478b
recursivelftr <recursivelftr@gmail.com>
2021-05-09 20:27:45 -0700

    Added code to auto detect gd installation and save folders

--- src/main/kotlin/io/grimlocations/util/extension/AppDirs.kt
+++ src/main/kotlin/io/grimlocations/util/extension/AppDirs.kt
@@ -1,8 +1,20 @@
 package io.grimlocations.util.extension
 
+import com.sun.jna.platform.win32.Advapi32Util
+import com.sun.jna.platform.win32.Shell32Util
+import com.sun.jna.platform.win32.ShlObj
+import com.sun.jna.platform.win32.WinReg
 import io.grimlocations.constant.TITLE
 import net.harawata.appdirs.AppDirs
+import org.apache.logging.log4j.LogManager
 import java.io.File
+
+private val logger = LogManager.getLogger()
+
+private var didFetchSteamInstallDir = false
+private var didFetchDocsDir = false
+private var steamInstallDir: String? = null
+private var documentsDir: String? = null
 
 val AppDirs.glDataDir: String
     get() = getUserDataDir(TITLE, null, null)
@@ -12,3 +24,41 @@ val AppDirs.glDatabaseDir: String
 
 val AppDirs.glDatabaseBackupDir: String
     get() = "$glDataDir${File.separator}database_backup"
+
+val AppDirs.gdInstallLocation: String?
+    get() = getSteamInstallLocation()?.let {
+        "$it${File.separator}steamapps${File.separator}common${File.separator}Grim Dawn"
+    }
+
+val AppDirs.gdSaveLocation: String?
+    get() = getDocumentsDirectory()?.let {
+        "$it${File.separator}My Games${File.separator}Grim Dawn${File.separator}save${File.separator}main"
+    }
+
+private fun getDocumentsDirectory(): String? {
+    try {
+        if (!didFetchDocsDir) {
+            didFetchDocsDir = true
+            documentsDir = Shell32Util.getFolderPath(ShlObj.CSIDL_PERSONAL)
+        }
+    } catch (e: Exception) {
+        logger.error("Unable to get the documents directory.", e)
+    }
+    return documentsDir
+}
+
+private fun getSteamInstallLocation(): String? {
+    try {
+        if (!didFetchSteamInstallDir) {
+            didFetchSteamInstallDir = true
+            steamInstallDir = Advapi32Util.registryGetStringValue(
+                WinReg.HKEY_LOCAL_MACHINE,
+                "SOFTWARE${File.separator}Wow6432Node${File.separator}Valve${File.separator}Steam",
+                "InstallPath"
+            )
+        }
+    } catch (e: Exception) {
+        logger.error("Unable to get the steam install folder from the registry.", e)
+    }
+    return steamInstallDir
+}
